6b89414eec1546d67caf4f1ba2f2cd9a4a620a9f
Jorge Garcia <xurdedix@gmail.com>
2012-05-10 13:12:14 +0200

    [#28661291]

---
+++ config/initializers/searcher.rb
@@ -0,0 +1,5 @@
+# -*- encoding : utf-8 -*-
+# Be sure to restart your server when you modify this file.
+
+Spree::Config.searcher_class = Spree::Search::SpreeSunspot
+puts Spree::Config.searcher_class 
